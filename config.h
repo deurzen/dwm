92737599062264a0759425e02a45672a7451ba42
deurzen <m.vandeurzen@student.ru.nl>
2018-03-15 20:25:05 +0100

    adds centeredmaster patch

--- config.h
+++ config.h
@@ -63,6 +63,8 @@ static const Layout layouts[] = {
   { "[\\]",     dwindle },
   { "TTT",      bstack },
   { "===",      bstackhoriz },
+  { "|M|",      centeredmaster },
+  { ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -128,6 +130,8 @@ static Key keys[] = {
   { MODKEY,                       XK_d,      setlayout,      {.v = &layouts[4]} },
   { MODKEY,                       XK_b,      setlayout,      {.v = &layouts[5]} },
   { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
+  { MODKEY,                       XK_c,      setlayout,      {.v = &layouts[7]} },
+  { MODKEY,                       XK_v,      setlayout,      {.v = &layouts[8]} },
   { MODKEY,                       XK_space,  setlayout,      {0} },
   { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
